214fe8c2a2f2b56d36a1cd1091431ea3df93140c
Murilo Silva <murilohenriqued746@gmail.com>
2023-08-04 18:45:55 -0300

    A saga continua

--- POO/Aula1/verificacaov2.js
+++ POO/Aula1/verificacaov2.js
@@ -1,19 +1,37 @@
 import promptsync from 'prompt-sync';
-const prompt = promptsync({sigint: true});
+const prompt = promptsync({ sigint: true });
 
-function acrescentaVerificador(matricula){
+function nivelAcessoOk(nivelAcesso){
+    switch(nivelAcesso){
+        case '21':
+        case '1':
+        case '35':
+        case '66':
+            return true;
+        default:
+            return false;
+        
+    }
+}
+function acrescentaVerificador(matricula, nivelAcesso=21) {
     let digitos = matricula;
     let soma = 0;
 
-    while(digitos.length != 1){
-        for(let i = 0;i< digitos.length;i++){
-            soma = soma + Number(digitos.at(i));
+    while (digitos.length != 1) {
+        for (let i = 0; i < digitos.length; i++) {
+            soma = soma + Number(digitos[i]);
         }
-    digitos = String(soma);
-    soma = 0
-    }   
-    matricula + '-'+ digitos;
+        digitos = String(soma);
+        soma = 0;
+    }
+    return nivelAcesso+matricula + '-' + digitos;
 }
-let matricula = prompt("Digite sua matricula: ")
-let mtrCompleta = acrescentaVerificador(matricula);
-console.log('Numero da matricula completo:',mtrCompleta)
+
+let matricula = prompt("Digite sua matricula: ");
+let nivel = prompt("Digite seu nivel de acesso:")
+if (nivelAcessoOk(nivel) == true){
+    let mtrCompleta = acrescentaVerificador(matricula);
+    console.log('Numero da matricula completo:', mtrCompleta);
+}else{
+    console.log("Inválido")
+}
